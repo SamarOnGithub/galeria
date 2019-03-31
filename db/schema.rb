2112bdf44e4ee64b9c2e34492276a2d147e09f28
admin <admin@admin.com>
2019-03-31 04:23:40 -0400

    changed db files to successfully attach ruby app to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160510222559) do
+ActiveRecord::Schema.define(version: 2016_05_10_222559) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "images", force: :cascade do |t|
+  create_table "images", id: :serial, force: :cascade do |t|
     t.string "title"
     t.string "url"
   end
